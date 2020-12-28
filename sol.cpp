47484572f6ac5fe2e32ec63496c54b60c8e3597c
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-28 20:03:39 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -9,7 +9,7 @@ int main() {
 	cin >> s;
 	if ((s[0] > 'a' && s[0] < 'h') && (s[1] > '1' && s[1] < '8')) {
 		// if the column is greater than 'a' but less than 'h', and the row is greater than '1' but less than '8'
-		// then, the total number of possible is moves is 8
+		// then, the total number of possible moves is 8
 		cout << 8;
 	} else if ((s[0] == 'a' || s[0] == 'h') && (s[1] == '1' || s[1] == '8')) {
 		// if the king is in the corner, then the total number of possible moves is 3
